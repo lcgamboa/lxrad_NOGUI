70d2d45eb7d8fb6c6e79796b44d33050673e43e3
lcgamboa <lcgamboa@yahoo.com>
2021-07-06 13:21:25 -0300

    chg: new function to get remote files !minor

--- include/lxutils.h
+++ include/lxutils.h
@@ -281,4 +281,6 @@ bool lxRenameFile(lxString oldfname, lxString newfname);
 unsigned int lxGetDisplayWidth(int disp);
 unsigned int lxGetDisplayHeight(int disp);
 
+lxString lxGetLocalFile(lxString file);
+
 #endif	/* LXUTILS_H */
